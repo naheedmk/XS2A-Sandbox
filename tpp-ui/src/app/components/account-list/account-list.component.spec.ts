2f32bb04ca3433185d151bfc13677368031cc4e5
Vladimir Nechai <vne@adorsys.de>
2019-05-02 15:14:51 +0300

    account-list tests

--- tpp-ui/src/app/components/account-list/account-list.component.spec.ts
+++ tpp-ui/src/app/components/account-list/account-list.component.spec.ts
@@ -2,19 +2,25 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AccountListComponent} from './account-list.component';
 import {RouterTestingModule} from "@angular/router/testing";
-import {HttpClientModule} from "@angular/common/http";
+import {AccountService} from "../../services/account.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {Account, AccountStatus, AccountType, UsageType} from "../../models/account.model";
+import {Observable} from "rxjs";
+import "rxjs-compat/add/observable/from";
 
 describe('AccountListComponent', () => {
     let component: AccountListComponent;
     let fixture: ComponentFixture<AccountListComponent>;
+    let accountService: AccountService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
                 RouterTestingModule,
-                HttpClientModule,
+                HttpClientTestingModule,
             ],
-            declarations: [AccountListComponent]
+            declarations: [AccountListComponent],
+            providers: [AccountService]
         })
             .compileComponents();
     }));
@@ -23,9 +29,39 @@ describe('AccountListComponent', () => {
         fixture = TestBed.createComponent(AccountListComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        accountService = TestBed.get(AccountService);
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should load accounts on NgOnInit', () => {
+        let mockAccounts: Account[] = [
+            {
+                id: 'XXXXXX',
+                iban: 'DE35653635635663',
+                bban: 'BBBAN',
+                pan: 'pan',
+                maskedPan: 'maskedPan',
+                currency: 'EUR',
+                msisdn: 'MSISDN',
+                name: 'Pupkin',
+                product: 'Deposit',
+                accountType: AccountType.CASH,
+                accountStatus: AccountStatus.ENABLED,
+                bic: 'BIChgdgd',
+                usageType: UsageType.PRIV,
+                details: '',
+                linkedAccounts: '',
+                balances: []
+            } as Account
+        ];
+        let getAccountsSpy = spyOn(accountService, 'getAccounts').and.returnValue(Observable.of(mockAccounts));
+
+        component.ngOnInit();
+
+        expect(getAccountsSpy).toHaveBeenCalled();
+        expect(component.accounts).not.toBeNull();
     });
 });
