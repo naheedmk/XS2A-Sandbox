f99b2d453967a4681fd74298fbeb8ed73c1cf90b
Collins Kamgaing <cka@adorsys.de>
2019-05-06 11:37:00 +0200

    implement user-create test

--- tpp-ui/src/app/components/users/user-create/user-create.component.spec.ts
+++ tpp-ui/src/app/components/users/user-create/user-create.component.spec.ts
@@ -1,13 +1,19 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormArray, ReactiveFormsModule} from "@angular/forms";
 import {RouterTestingModule} from "@angular/router/testing";
 import {HttpClientModule} from "@angular/common/http";
 import {UserCreateComponent} from './user-create.component';
 import {IconModule} from "../../../commons/icon/icon.module";
+import {DebugElement} from "@angular/core";
+import {UserService} from "../../../services/user.service";
+import {Observable} from "rxjs";
 
 describe('UserCreateComponent', () => {
     let component: UserCreateComponent;
     let fixture: ComponentFixture<UserCreateComponent>;
+    let userService: UserService;
+    let de: DebugElement;
+    let el: HTMLElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -17,6 +23,7 @@ describe('UserCreateComponent', () => {
                 HttpClientModule,
                 IconModule
             ],
+            providers: [UserService],
             declarations: [UserCreateComponent]
         })
             .compileComponents();
@@ -30,5 +37,121 @@ describe('UserCreateComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('submitted should false', () => {
+        expect(component.submitted).toBeFalsy();
+    });
+
+    it('userForm should be invalid when at least one field is empty', () => {
+        expect(component.userForm.valid).toBeFalsy();
+    });
+
+    it('email field validity', () => {
+        let errors = {};
+        const email = component.userForm.controls['email'];
+        expect(email.valid).toBeFalsy();
+
+        // email field is required
+        errors = email.errors || {};
+        expect(errors['required']).toBeTruthy();
+
+        // set email to something incorrect
+        email.setValue('testtests.de');
+        errors = email.errors || {};
+        expect(errors['email']).toBeTruthy();
+
+        // set email to something correct
+        email.setValue('test@test.de');
+        errors = email.errors || {};
+        expect(errors['required']).toBeFalsy();
+    });
+
+    it('login field validity', () => {
+        let errors = {};
+        const login = component.userForm.controls['login'];
+        expect(login.valid).toBeFalsy();
+
+        // login field is required
+        errors = login.errors || {};
+        expect(errors['required']).toBeTruthy();
+
+        // set login to something correct
+        login.setValue('test@test.de');
+        errors = login.errors || {};
+        expect(errors['required']).toBeFalsy();
+    });
+
+    it('pin field validity', () => {
+        let errors = {};
+        const pin = component.userForm.controls['pin'];
+        expect(pin.valid).toBeFalsy();
+
+        // pin field is required
+        errors = pin.errors || {};
+        expect(errors['required']).toBeTruthy();
+
+        // pin should have at least 5 characters
+        pin.setValue('1234');
+        errors = pin.errors || {};
+        expect(errors['required']).toBeFalsy();
+        expect(errors['minlength']).toBeTruthy();
+
+        // set pin to something correct
+        pin.setValue('12345678');
+        errors = pin.errors || {};
+        expect(errors['required']).toBeFalsy();
+    });
+
+    it('SCA validity', () => {
+        let errors = {};
+        const sca = component.userForm.controls['scaUserData']['controls'][0].controls['methodValue'];
+        expect(sca.valid).toBeFalsy();
+
+        // pin field is required
+        errors = sca.errors || {};
+        expect(errors['required']).toBeTruthy();
+
+        // set pin to something correct
+        sca.setValue('sca method');
+        errors = sca.errors || {};
+        expect(errors['required']).toBeFalsy();
+    });
+
+    it('validate onSubmit method', () => {
+        component.onSubmit();
+        expect(component.submitted).toEqual(true);
+        expect(component.userForm.valid).toBeFalsy();
+    });
+
+    it('validate setupUserFormControl method', () => {
+        component.setupUserFormControl();
+        expect(component.userForm).toBeDefined();
+    });
+
+    it('validate formControl method', () => {
+        expect(component.formControl).toEqual(component.userForm.controls);
+    });
+
+    it('validate addScaData method', () => {
+        const length = (<FormArray>component.userForm.controls['scaUserData']).length;
+        component.addScaDataItem();
+        const newLength =  (<FormArray>component.userForm.controls['scaUserData']).length;
+        expect(newLength).toEqual(length + 1);
+    });
+
+    it('validate removeScaDataItem method', () => {
+        component.removeScaDataItem(0);
+        const length =  (<FormArray>component.userForm.controls['scaUserData']).length;
+        expect(length).toEqual(0);
+    });
+
+    it('validate iniScaData method', () => {
+        const formGroup = component.initScaData();
+        const data = {
+            scaMethod: 'EMAIL',
+            methodValue: ''
+        };
+        expect(formGroup.value).toEqual(data);
     });
 });
