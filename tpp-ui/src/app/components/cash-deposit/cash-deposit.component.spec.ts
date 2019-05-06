397318da7fdd8eefd70305cc0d3435a211063def
Vladimir Nechai <vne@adorsys.de>
2019-05-06 17:00:51 +0200

    deposit cash test

--- tpp-ui/src/app/components/cash-deposit/cash-deposit.component.spec.ts
+++ tpp-ui/src/app/components/cash-deposit/cash-deposit.component.spec.ts
@@ -3,18 +3,27 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CashDepositComponent} from './cash-deposit.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {RouterTestingModule} from "@angular/router/testing";
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {AccountService} from "../../services/account.service";
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 describe('CashDepositComponent', () => {
     let component: CashDepositComponent;
     let fixture: ComponentFixture<CashDepositComponent>;
+    let accountService: AccountService;
+    let router: Router;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
                 ReactiveFormsModule,
                 RouterTestingModule,
-                HttpClientModule,
+                HttpClientTestingModule,
+                RouterTestingModule.withRoutes([])
+            ],
+            providers: [
+                AccountService
             ],
             declarations: [CashDepositComponent]
         })
@@ -24,10 +33,30 @@ describe('CashDepositComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(CashDepositComponent);
         component = fixture.componentInstance;
+        accountService = TestBed.get(AccountService);
+        router = TestBed.get(Router);
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should deposit cash when form is valid and submitted', () => {
+        expect(component.submitted).toBeFalsy();
+
+        // set valid values for form
+        component.form.controls['currency'].setValue('EUR');
+        component.form.controls['amount'].setValue('50');
+
+        // form submit
+        const sampleResponse = {value: 'sample response'};
+        let depositCashSpy = spyOn(accountService, 'depositCash').and.callFake(() => Observable.of(sampleResponse));
+        let navigateSpy = spyOn(router, 'navigate');
+        component.onSubmit();
+        expect(component.submitted).toBeTruthy();
+        expect(component.form.valid).toBeTruthy();
+        expect(depositCashSpy).toHaveBeenCalled();
+        expect(navigateSpy).toHaveBeenCalledWith(['/accounts']);
     });
 });
