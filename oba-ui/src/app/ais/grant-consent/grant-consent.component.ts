0619676c21992891ca4ff021ea0f23e49eeac2d4
Vladimir Nechai <vne@adorsys.de>
2019-05-15 19:49:59 +0200

    refactor ais, pis and payment-cancellation html for scroll

--- oba-ui/src/app/ais/grant-consent/grant-consent.component.ts
+++ oba-ui/src/app/ais/grant-consent/grant-consent.component.ts
@@ -15,153 +15,7 @@ import {AccountDetailsTO} from "../../api/models/account-details-to";
 })
 export class GrantConsentComponent implements OnInit, OnDestroy {
 
-  public authResponse: ConsentAuthorizeResponse = {
-    "psuMessages": [],
-    "encryptedConsentId": "e5nrDmqUJHzp9v28MkZxnb7ItZhCULlZBLQpIgPF0bHKDHnIh3ueFr3W2z92MLLRuWhWk2-WNYnbWdFPTsl_Ig==_=_bS6p6XvTWI",
-    "scaMethods": [
-      {
-        "id": "YNJjHF67TRgt2Gty9Pj1XU",
-        "scaMethod": "EMAIL",
-        "methodValue": "12345678_defaultPsu.private@mail.de"
-      }
-    ],
-    "authorisationId": "a2906505-ea43-4c2a-a9f4-adcc111b0dc7",
-    "scaStatus": "psuIdentified",
-    "accounts": [
-      {
-        "id": "L7eoFzGpQEwu0sgYFR0u9w",
-        "iban": "DE38760700241234567801",
-        "bban": null,
-        "pan": null,
-        "maskedPan": null,
-        "msisdn": null,
-        "currency": "EUR",
-        "name": "AcctPsuDefPriv1",
-        "product": "Cash24",
-        "accountType": "CASH",
-        "accountStatus": "ENABLED",
-        "bic": null,
-        "linkedAccounts": null,
-        "usageType": "PRIV",
-        "details": "Private PSU that is used to trigger default behaviour of the ASPSP",
-        "balances": [
-          {
-            "amount": {
-              "currency": "EUR",
-              "amount": 190.55
-            },
-            "balanceType": "INTERIM_AVAILABLE",
-            "lastChangeDateTime": "2019-05-15T10:09:19.815",
-            "referenceDate": "2019-05-15",
-            "lastCommittedTransaction": null
-          }
-        ]
-      },
-      {
-        "id": "BvitSBedTDglLy-8XLib-A",
-        "iban": "DE11760700241234567802",
-        "bban": null,
-        "pan": null,
-        "maskedPan": null,
-        "msisdn": null,
-        "currency": "EUR",
-        "name": "AcctPsuDefPriv2",
-        "product": "Cash24",
-        "accountType": "CASH",
-        "accountStatus": "ENABLED",
-        "bic": null,
-        "linkedAccounts": null,
-        "usageType": "PRIV",
-        "details": "Private PSU that is used to trigger default behaviour of the ASPSP",
-        "balances": [
-          {
-            "amount": {
-              "currency": "EUR",
-              "amount": 382.64
-            },
-            "balanceType": "INTERIM_AVAILABLE",
-            "lastChangeDateTime": "2019-05-15T10:09:19.924",
-            "referenceDate": "2019-05-15",
-            "lastCommittedTransaction": null
-          }
-        ]
-      },
-      {
-        "id": "yXLpZZKmQGcr5T7ofUEjfk",
-        "iban": "DE81760700241234567803",
-        "bban": null,
-        "pan": null,
-        "maskedPan": null,
-        "msisdn": null,
-        "currency": "EUR",
-        "name": "AcctPsuDefPriv3",
-        "product": "Cash24",
-        "accountType": "CASH",
-        "accountStatus": "ENABLED",
-        "bic": null,
-        "linkedAccounts": null,
-        "usageType": "PRIV",
-        "details": "Private PSU that is used to trigger default behaviour of the ASPSP",
-        "balances": [
-          {
-            "amount": {
-              "currency": "EUR",
-              "amount": 231.67
-            },
-            "balanceType": "INTERIM_AVAILABLE",
-            "lastChangeDateTime": "2019-05-15T10:09:20.034",
-            "referenceDate": "2019-05-15",
-            "lastCommittedTransaction": null
-          }
-        ]
-      },
-      {
-        "id": "UJ-Wm5EqRR8vEpJYZwu_vA",
-        "iban": "DE54760700241234567804",
-        "bban": null,
-        "pan": null,
-        "maskedPan": null,
-        "msisdn": null,
-        "currency": "EUR",
-        "name": "AcctPsuDefPriv4",
-        "product": "Cash24",
-        "accountType": "CASH",
-        "accountStatus": "ENABLED",
-        "bic": null,
-        "linkedAccounts": null,
-        "usageType": "PRIV",
-        "details": "Private PSU that is used to trigger default behaviour of the ASPSP",
-        "balances": [
-          {
-            "amount": {
-              "currency": "EUR",
-              "amount": 650.88
-            },
-            "balanceType": "INTERIM_AVAILABLE",
-            "lastChangeDateTime": "2019-05-15T10:09:20.133",
-            "referenceDate": "2019-05-15",
-            "lastCommittedTransaction": null
-          }
-        ]
-      }
-    ],
-    "consent": {
-      "id": "1f734e5a-9727-4421-96b5-89a208156d43",
-      "userId": "12345678_Default-PSU-Private",
-      "tppId": "PSDDE-FAKENCA-87B2AC",
-      "frequencyPerDay": 5,
-      "access": {
-        "accounts": [],
-        "balances": [],
-        "transactions": [],
-        "availableAccounts": null,
-        "allPsd2": "ALL_ACCOUNTS"
-      },
-      "validUntil": "2019-10-10",
-      "recurringIndicator": true
-    },
-    "authMessageTemplate": null
-  };
+  public authResponse: ConsentAuthorizeResponse;
   public encryptedConsentId: string;
   public authorisationId: string;
   public bankOfferedForm: FormGroup;
@@ -199,16 +53,14 @@ export class GrantConsentComponent implements OnInit, OnDestroy {
   }
 
   public ngOnInit(): void {
-    console.log(this.isBankOfferedConsent());
-    this.bankOffered = this.isBankOfferedConsent();
-    // this.shareService.currentData.subscribe(data => {
-    //   if (data) {
-    //     this.shareService.currentData.subscribe(authResponse => {
-    //       this.authResponse = authResponse;
-    //       this.bankOffered = this.isBankOfferedConsent();
-    //     });
-    //   }
-    // });
+    this.shareService.currentData.subscribe(data => {
+      if (data) {
+        this.shareService.currentData.subscribe(authResponse => {
+          this.authResponse = authResponse;
+          this.bankOffered = this.isBankOfferedConsent();
+        });
+      }
+    });
   }
 
   public onSubmit() {
