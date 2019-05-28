75924adf487016b41b5a9c96649d048968461471
Yurii Averin <averin.yury@gmail.com>
2019-05-28 12:43:49 +0300

    #83 Developer portal. UI Customization
    Add login page
    Add logout and settings icons

--- developer-portal-ui/src/app/app.component.ts
+++ developer-portal-ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../services/data.service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent implements OnInit {
   constructor(
     private router: Router,
     private actRoute: ActivatedRoute,
-    public dataService: DataService
+    public dataService: DataService,
+    public loginService: LoginService
   ) {}
 
   goToPage(page) {
