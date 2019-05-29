890cd4815417b5af9d7e4da4de79d9eea095a302
Vladimir Nechai <vne@adorsys.de>
2019-05-29 17:20:33 +0200

    add extra error messages for different error codes

--- tpp-ui/src/app/interceptors/global-errors-handler.ts
+++ tpp-ui/src/app/interceptors/global-errors-handler.ts
@@ -6,55 +6,69 @@ import {InfoService} from "../commons/info/info.service";
 @Injectable()
 export class GlobalErrorsHandler implements ErrorHandler {
 
-  constructor(
-    private zone: NgZone,
-    private injector: Injector) {
-  }
+    constructor(
+        private zone: NgZone,
+        private injector: Injector) {
+    }
 
-  public get router(): Router {
-    return this.injector.get(Router);
-  }
+    public get router(): Router {
+        return this.injector.get(Router);
+    }
 
-  public get infoService(): InfoService {
-    return this.injector.get(InfoService);
-  }
+    public get infoService(): InfoService {
+        return this.injector.get(InfoService);
+    }
 
-  public get activatedRoute(): ActivatedRoute {
-    return this.injector.get(ActivatedRoute);
-  }
+    public get activatedRoute(): ActivatedRoute {
+        return this.injector.get(ActivatedRoute);
+    }
 
-  public handleError(error: HttpErrorResponse) {
-    console.error("TPP UI error handler: ", error);
+    public handleError(error: HttpErrorResponse) {
+        console.error("TPP UI error handler: ", error);
 
-    let httpErrorCode = error.status;
+        let httpErrorCode = error.status;
 
-    this.zone.run( () => {
+        console.log(httpErrorCode);
 
-      // default error handling
-      switch (httpErrorCode) {
-        case 401: {
-          this.infoService.openFeedback('Invalid credentials', {
-            severity: 'error'
-          });
-          break;
-        }
-        case 403: {
-          this.infoService.openFeedback('You have no rights to execute this action', {
-            severity: 'error'
-          });
-          break;
-        }
-        default: {
-          // if required could be redirected to internal server error page
-          this.infoService.openFeedback('Internal server error. Please contact technical support', {
-            severity: 'error'
-          });
-          break;
-        }
-      }
+        this.zone.run(() => {
 
-    });
+            // default error handling
+            switch (httpErrorCode) {
+                case 401: {
+                    this.infoService.openFeedback('Invalid authentication credentials', {
+                        severity: 'error'
+                    });
+                    break;
+                }
+                case 403: {
+                    this.infoService.openFeedback('You have no rights to execute this action', {
+                        severity: 'error'
+                    });
+                    break;
+                }
+                case 404: {
+                    this.infoService.openFeedback('Required resource not found', {
+                        severity: 'error'
+                    });
+                    break;
+                }
+                case 409: {
+                    this.infoService.openFeedback('Conflict. A record with the given email or login already exists.', {
+                        severity: 'error'
+                    });
+                    break;
+                }
+                default: {
+                    // if required could be redirected to internal server error page
+                    this.infoService.openFeedback('Internal server error. Please contact technical support', {
+                        severity: 'error'
+                    });
+                    break;
+                }
+            }
 
-  }
+        });
+
+    }
 
 }
