c5c57cdc3a72c2c2ab60694983b92e08f5db1c85
Yurii Averin <averin.yury@gmail.com>
2019-05-31 16:57:18 +0300

    #83 Developer portal. UI Customization

--- developer-portal-ui/src/app/app.component.ts
+++ developer-portal-ui/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 import { LoginService } from '../services/login.service';
-import { ConstantService } from '../services/constant.service';
+import {
+  CustomizeService,
+  GlobalSettings,
+} from '../services/customize.service';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +14,21 @@ import { ConstantService } from '../services/constant.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  globalSettings: GlobalSettings;
+  defaultSettings: GlobalSettings;
+  headerBG: string;
+  headerFontColor: string;
+  footerBG: string;
+  footerFontColor: string;
+  previousStatus = false;
+
   constructor(
     private router: Router,
     private actRoute: ActivatedRoute,
     public dataService: DataService,
     public loginService: LoginService,
-    public constantService: ConstantService
+    public customizeService: CustomizeService,
+    private localStorageService: LocalStorageService
   ) {}
 
   goToPage(page) {
@@ -28,5 +41,46 @@ export class AppComponent implements OnInit {
     ].snapshot.url;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    let theme;
+    if (!this.localStorageService.get('defaultTheme')) {
+      theme = this.customizeService.getTheme();
+      this.localStorageService.set('defaultTheme', theme);
+    }
+    if (this.localStorageService.get('userTheme')) {
+      theme = this.localStorageService.get('userTheme');
+      this.customizeService.setUserTheme(theme);
+    } else {
+      theme = this.localStorageService.get('defaultTheme');
+    }
+    this.customizeService.changeFontFamily();
+    this.globalSettings = theme.globalSettings;
+    this.defaultSettings = this.customizeService.getDefaultTheme().globalSettings;
+
+    this.headerBG = this.defaultSettings.headerBG;
+    this.headerFontColor = this.defaultSettings.headerFontColor;
+    this.footerBG = this.defaultSettings.footerBG;
+    this.footerFontColor = this.defaultSettings.footerFontColor;
+
+    setInterval(() => {
+      if (this.customizeService.getChangeStatus() !== this.previousStatus) {
+        console.log('works');
+        this.previousStatus = this.customizeService.getChangeStatus();
+        theme = this.localStorageService.get('userTheme')
+          ? this.localStorageService.get('userTheme')
+          : this.customizeService.getNewThemaStatus()
+          ? this.customizeService.getTheme()
+          : this.localStorageService.get('defaultTheme');
+        this.globalSettings = theme.globalSettings;
+        if (this.globalSettings.headerBG) {
+          this.headerBG = this.globalSettings.headerBG;
+          this.headerFontColor = this.globalSettings.headerFontColor;
+        }
+        if (this.globalSettings.footerBG) {
+          this.footerBG = this.globalSettings.footerBG;
+          this.footerFontColor = this.globalSettings.footerFontColor;
+        }
+      }
+    }, 500);
+  }
 }
