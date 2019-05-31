36f5fcb07a27676656cf0225f4395b982b8262c1
Yurii Averin <averin.yury@gmail.com>
2019-05-31 16:57:18 +0300

    #83 Developer portal. UI Customization

--- developer-portal-ui/src/pages/settigs-modal/settings-modal.component.ts
+++ developer-portal-ui/src/pages/settigs-modal/settings-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { ConstantService } from '../../services/constant.service';
+import { CustomizeService, Theme } from '../../services/customize.service';
+import { DataService } from '../../services/data.service';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-settings-modal',
@@ -7,29 +10,124 @@ import { ConstantService } from '../../services/constant.service';
   styleUrls: ['./settings-modal.component.scss'],
 })
 export class SettingsModalComponent {
-  openFlag: boolean;
-  newLogoPath: string;
+  importedTheme: Theme;
 
-  constructor(private constantService: ConstantService) {
-    this.newLogoPath = '';
+  constructor(
+    private customizeService: CustomizeService,
+    private dataService: DataService,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  save() {
+    if (!!this.importedTheme) {
+      this.customizeService.setUserTheme(this.importedTheme);
+      this.localStorageService.set('userTheme', this.importedTheme);
+      this.collapseThis();
+    }
   }
 
   close() {
-    this.openFlag = false;
+    this.customizeService.setDefaultTheme();
+    this.collapseThis('modal');
   }
 
-  save() {
-    this.constantService.setLogo(this.newLogoPath);
-    this.collapseThis();
+  onFileChanged(event) {
+    const fileReader = new FileReader();
+    fileReader.readAsText(event.target.files[0], 'UTF-8');
+    fileReader.onload = () => {
+      if (typeof fileReader.result === 'string') {
+        try {
+          this.importedTheme = JSON.parse(fileReader.result);
+          const errors = this.validateTheme(this.importedTheme);
+          if (errors.length !== 0) {
+            this.dataService.showToast(
+              JSON.stringify(errors),
+              'Error!',
+              'error'
+            );
+          } else {
+            this.customizeService.setUserTheme(this.importedTheme);
+          }
+        } catch (e) {
+          this.dataService.showToast('File is not valid!', 'Error!', 'error');
+          console.log('invalid json file', e);
+        }
+      } else {
+        this.dataService.showToast("It's not a JSON file", 'Error!', 'error');
+      }
+    };
+    fileReader.onerror = error => {
+      console.log(error);
+    };
   }
 
-  collapseThis() {
-    const collapsibleItemContent = document.getElementById('modal');
+  onFileClear(event) {
+    event.target.value = null;
+  }
 
-    if (collapsibleItemContent.style.maxHeight) {
-      collapsibleItemContent.style.maxHeight = '';
+  collapseThis(id?: string) {
+    if (id === 'modal') {
+      const collapsibleItemContent = document.getElementById(id);
+
+      if (collapsibleItemContent.style.maxHeight) {
+        collapsibleItemContent.style.maxHeight = '';
+      } else {
+        collapsibleItemContent.style.maxHeight = '20vw';
+      }
     } else {
-      collapsibleItemContent.style.maxHeight = '20vw';
+      const collapsibleItemContent = document.getElementById(id);
+
+      if (collapsibleItemContent.style.maxHeight) {
+        collapsibleItemContent.style.maxHeight = '';
+      } else {
+        collapsibleItemContent.style.maxHeight = '5vw';
+      }
     }
+  }
+
+  exportTheme(type: string) {
+    let tmp;
+    if (this.localStorageService.get(type)) {
+      tmp = this.localStorageService.get(type);
+    } else {
+      type = 'defaultTheme';
+      tmp = this.localStorageService.get(type);
+    }
+    const blob = new Blob([JSON.stringify(tmp, null, 2)], {
+      type: 'application/json',
+    });
+    saveAs(blob, type);
+  }
+
+  validateTheme(theme) {
+    const general = ['globalSettings', 'contactInfo', 'officesInfo'];
+    const additional = [
+      ['logoPath'],
+      ['imgPath', 'name', 'position'],
+      ['city', 'company', 'addressFirstLine', 'addressSecondLine'],
+    ];
+    const errors: string[] = [];
+
+    for (let i = 0; i < general.length; i++) {
+      if (!theme.hasOwnProperty(general[i])) {
+        errors.push(`Missing field ${general[i]}!`);
+      } else if (i !== 2) {
+        for (const property of additional[i]) {
+          if (!theme[general[i]].hasOwnProperty(property)) {
+            errors.push(`Field ${general[i]} missing property ${property}!`);
+          }
+        }
+      } else {
+        for (const office of theme.officesInfo) {
+          for (const property of additional[i]) {
+            if (!office.hasOwnProperty(property)) {
+              errors.push(`Field ${general[i]} missing property ${property}!`);
+            }
+          }
+        }
+      }
+    }
+
+    return errors;
   }
 }
