a77d9f8d10484b4ca36724db8c1287324097081f
Vladimir Nechai <vne@adorsys.de>
2019-06-19 14:18:00 +0200

    throw session expire error

--- tpp-ui/src/app/commons/navbar/navbar.component.ts
+++ tpp-ui/src/app/commons/navbar/navbar.component.ts
@@ -1,9 +1,6 @@
 import {Component, DoCheck, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {Subscription} from "rxjs";
-import {AutoLogoutService} from "../../services/auto-logout.service";
-import {InfoService} from "../info/info.service";
-import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
     selector: 'app-navbar',
@@ -14,42 +11,20 @@ export class NavbarComponent implements OnInit, DoCheck {
 
     subscription = new Subscription();
 
-    constructor(private authService: AuthService,
-                private autoLogoutService: AutoLogoutService,
-                private infoService: InfoService) {
+    constructor(private authService: AuthService) {
     }
 
     ngOnInit() {
-        // this.startTokenMonitoring();
-    }
-
-    // subscribe every minute and check if token is still valid
-    startTokenMonitoring(): void {
-        this.subscription = this.autoLogoutService.timerSubject.subscribe(time => {
-            if (!this.authService.isLoggedIn()) {
-                this.onLogout();
-            }
-        })
     }
 
     onLogout(): void {
         this.authService.logout();
     }
 
-    ngOnDestroy(): void {
-        this.subscription.unsubscribe();
-    }
-
     ngDoCheck(): void {
         if (!this.authService.isLoggedIn()) {
-            console.log("expired");
             this.authService.logout();
-            throw new HttpErrorResponse({error: new Error("Session Expired"), status: 403, statusText: 'Your session expired'});
-
-            // this.infoService.openFeedback("Your session has expired", {
-            //     severity: 'error'
-            // });
-
+            throw new Error('Session expired. Please login again.');
         }
     }
 }
