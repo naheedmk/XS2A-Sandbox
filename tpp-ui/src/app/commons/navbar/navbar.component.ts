62ed106880fa59d0e5950886bbba277fe5494870
Vladimir Nechai <vne@adorsys.de>
2019-06-19 12:24:37 +0200

    doCheck hook in navbar component, destroy subscription

--- tpp-ui/src/app/commons/navbar/navbar.component.ts
+++ tpp-ui/src/app/commons/navbar/navbar.component.ts
@@ -1,29 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {Subscription} from "rxjs";
 import {AutoLogoutService} from "../../services/auto-logout.service";
+import {InfoService} from "../info/info.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, DoCheck {
 
     subscription = new Subscription();
 
-    constructor(private authService: AuthService, private autoLogoutService: AutoLogoutService) {
+    constructor(private authService: AuthService,
+                private autoLogoutService: AutoLogoutService,
+                private infoService: InfoService) {
     }
 
     ngOnInit() {
-        this.startTokenMonitoring();
+        // this.startTokenMonitoring();
     }
 
     // subscribe every minute and check if token is still valid
     startTokenMonitoring(): void {
         this.subscription = this.autoLogoutService.timerSubject.subscribe(time => {
             if (!this.authService.isLoggedIn()) {
-              this.onLogout();
+                this.onLogout();
             }
         })
     }
@@ -34,5 +38,18 @@ export class NavbarComponent implements OnInit {
 
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
+    }
+
+    ngDoCheck(): void {
+        if (!this.authService.isLoggedIn()) {
+            console.log("expired");
+            this.authService.logout();
+            throw new HttpErrorResponse({error: new Error("Session Expired"), status: 403, statusText: 'Your session expired'});
+
+            // this.infoService.openFeedback("Your session has expired", {
+            //     severity: 'error'
+            // });
+
+        }
     }
 }
