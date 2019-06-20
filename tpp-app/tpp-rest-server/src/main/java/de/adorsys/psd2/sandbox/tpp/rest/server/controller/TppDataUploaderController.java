7f3ce01f37105ef275bf6356e1b78cef2fe50e66
Dmitry Mishchuk <dmi@adorsys.com.ua>
2019-06-20 20:41:38 +0300

    fixed pmd

--- tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/controller/TppDataUploaderController.java
+++ tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/controller/TppDataUploaderController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
-import java.util.Optional;
 
 @Slf4j
 @RestController
