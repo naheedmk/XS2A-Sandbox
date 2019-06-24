cbef976d3488eedec553664f8e1dffa32fe0abc2
Vladimir Nechai <vne@adorsys.de>
2019-06-24 17:46:38 +0200

    list all users

--- tpp-app/tpp-rest-api/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/api/resource/TppUsersRestApi.java
+++ tpp-app/tpp-rest-api/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/api/resource/TppUsersRestApi.java
@@ -5,8 +5,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Api(tags = "TPP Users management")
 public interface TppUsersRestApi {
@@ -17,4 +20,10 @@ public interface TppUsersRestApi {
         authorizations = @Authorization(value = "apiKey"))
     @PostMapping
     ResponseEntity<UserTO> createUser(@RequestBody UserTO account);
+
+    @ApiOperation(value = "List users for a given TPP",
+        notes = "Endpoint to lists users for a given TPP",
+        authorizations = @Authorization(value = "apiKey"))
+    @GetMapping
+    ResponseEntity<List<UserTO>> getAllUsers();
 }
