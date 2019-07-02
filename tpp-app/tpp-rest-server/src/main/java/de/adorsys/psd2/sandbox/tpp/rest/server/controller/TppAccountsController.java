8999f20d7c780f3049a4c64590a3790bd862e55d
Vladimir Nechai <vne@adorsys.de>
2019-07-02 17:40:45 +0200

    create account with TTP deposit account class

--- tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/controller/TppAccountsController.java
+++ tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/controller/TppAccountsController.java
@@ -3,8 +3,12 @@ package de.adorsys.psd2.sandbox.tpp.rest.server.controller;
 import de.adorsys.ledgers.middleware.api.domain.account.AccountDetailsTO;
 import de.adorsys.ledgers.middleware.api.domain.payment.AmountTO;
 import de.adorsys.ledgers.middleware.client.rest.AccountMgmtStaffRestClient;
+import de.adorsys.psd2.sandbox.tpp.rest.api.domain.DepositAccount;
 import de.adorsys.psd2.sandbox.tpp.rest.api.resource.TppAccountsRestApi;
+import de.adorsys.psd2.sandbox.tpp.rest.server.mapper.AccountMapper;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +19,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping(TppAccountsRestApi.BASE_PATH)
 public class TppAccountsController implements TppAccountsRestApi {
+    private final AccountMapper accountMapper = Mappers.getMapper(AccountMapper.class);
     private final AccountMgmtStaffRestClient accountMgmtStaffRestClient;
 
     @Override
-    public ResponseEntity<Void> createAccount(String userId, AccountDetailsTO account) {
-        return accountMgmtStaffRestClient.createDepositAccountForUser(userId, account);
+    public ResponseEntity<Void> createAccount(String userId, DepositAccount account) {
+        return accountMgmtStaffRestClient.createDepositAccountForUser(userId, accountMapper.accountToAccountDetailsTO(account));
     }
 
     @Override
