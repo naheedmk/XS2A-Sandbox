f82e43ee71e199a4c670e2840fd61f47e213a06e
Vladimir Nechai <vne@adorsys.de>
2019-07-02 19:18:09 +0200

    remove excessive methods to fix pmd

--- tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/mapper/AccountMapper.java
+++ tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/mapper/AccountMapper.java
@@ -8,5 +8,4 @@ import org.mapstruct.Mapper;
 public interface AccountMapper {
 
     AccountDetailsTO accountToAccountDetailsTO (DepositAccount depositAccount);
-    DepositAccount accountDetailsTOtoDepositAccount (AccountDetailsTO accountDetailsTO);
 }
