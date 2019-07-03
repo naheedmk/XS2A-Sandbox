33631bda19d35b955804a6041bbc28154b5fb938
Yurii Averin <averin.yury@gmail.com>
2019-07-03 15:17:17 +0300

    #159 finish 'de' translation.

--- developer-portal-ui/src/services/language.service.ts
+++ developer-portal-ui/src/services/language.service.ts
@@ -5,7 +5,7 @@ import { TranslateService } from '@ngx-translate/core';
   providedIn: 'root',
 })
 export class LanguageService {
-  lang = 'es';
+  lang = 'en';
 
   constructor(private translateService: TranslateService) {}
 
