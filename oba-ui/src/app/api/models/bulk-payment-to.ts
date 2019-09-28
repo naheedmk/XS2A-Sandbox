28d5132e5f60a5cc059d3f15ccb20c99ba8fd1d6
Christian <noubichristian@yahoo.fr>
2019-09-28 13:01:40 +0200

    TPP-UI.  display bulk payment in redirect flow Ticket#371

--- oba-ui/src/app/api/models/bulk-payment-to.ts
+++ oba-ui/src/app/api/models/bulk-payment-to.ts
@@ -1,7 +1,11 @@
 /* tslint:disable */
 import { AccountReferenceTO } from './account-reference-to';
 import { SinglePaymentTO } from './single-payment-to';
+import {AmountTO} from "./amount-to";
 export interface BulkPaymentTO {
+  creditorAccount?: AccountReferenceTO;
+  creditorName?: string;
+  instructedAmount?: AmountTO;
   batchBookingPreferred?: boolean;
   debtorAccount?: AccountReferenceTO;
   paymentId?: string;
