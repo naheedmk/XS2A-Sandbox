304c812960f9ce56bd9467452fa4aafad1981bcb
Vladimir Nechai <vne@adorsys.de>
2019-10-09 17:26:55 +0200

    oauth service with auth component

--- oba-ui/src/app/auth/authorize/authorize.component.spec.ts
+++ oba-ui/src/app/auth/authorize/authorize.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthorizeComponent } from './authorize.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterTestingModule} from "@angular/router/testing";
+import {InfoModule} from "../../common/info/info.module";
 
 describe('AuthorizeComponent', () => {
   let component: AuthorizeComponent;
@@ -8,6 +11,7 @@ describe('AuthorizeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, RouterTestingModule, InfoModule],
       declarations: [ AuthorizeComponent ]
     })
     .compileComponents();
--- oba-ui/src/app/auth/authorize/authorize.component.ts
+++ oba-ui/src/app/auth/authorize/authorize.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {CustomizeService} from "../../common/services/customize.service";
+import {OnlineBankingOauthAuthorizationService} from "../../api/services/online-banking-oauth-authorization.service";
+import {OauthService} from "../services/oauth.service";
+import OauthCodeUsingPOSTParams = OnlineBankingOauthAuthorizationService.OauthCodeUsingPOSTParams;
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-authorize',
@@ -11,22 +15,43 @@ import {CustomizeService} from "../../common/services/customize.service";
 export class AuthorizeComponent implements OnInit {
 
   authorizeForm: FormGroup;
+  private redirectUri: string;
 
   constructor(
     public customizeService: CustomizeService,
+    private oAuthService: OauthService,
     private formBuilder: FormBuilder,
-    private router: Router
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.redirectUri = params.redirect_uri;
+
+      if (params.redirect_uri == undefined) {
+        this.router.navigate(['']);
+      }
+    });
+
     this.initAuthorizeForm();
+  }
+
+  public onSubmit(): void {
+    this.oAuthService.authorize({
+      ...this.authorizeForm.value,
+      redirectUri: this.redirectUri,
+    } as OauthCodeUsingPOSTParams)
+      .pipe(
+        map(response => response.redirectUri)
+      )
+      .subscribe(url => window.location.href = url);
   }
 
   private initAuthorizeForm(): void {
     this.authorizeForm = this.formBuilder.group({
       login: ['', Validators.required],
-      pin: ['', Validators.required],
-      redirectUri: ['', Validators.required]
+      pin: ['', Validators.required]
     });
   }
 
