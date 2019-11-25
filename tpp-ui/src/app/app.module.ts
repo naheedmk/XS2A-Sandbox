ace0c0dc19032da2bbfd4c642f327eab248de266
Christian <noubichristian@yahoo.fr>
2019-11-25 10:10:57 +0100

    Tpp-UI dropdown list close by clicking away#402

--- tpp-ui/src/app/app.module.ts
+++ tpp-ui/src/app/app.module.ts
@@ -45,6 +45,7 @@ import { ConvertBalancePipe } from './pipes/convertBalance.pipe';
 import { AutoLogoutService } from './services/auto-logout.service';
 import { SettingsHttpService } from './services/settings-http.service';
 import { UploadFileComponent } from './uploadFile/uploadFile.component';
+import {ClickOutsideModule} from "ng-click-outside";
 
 export function app_Init(settingsHttpService: SettingsHttpService) {
   return () => settingsHttpService.initializeApp();
@@ -88,6 +89,7 @@ export function app_Init(settingsHttpService: SettingsHttpService) {
     ReactiveFormsModule,
     IconModule,
     InfoModule,
+    ClickOutsideModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FileUploadModule,
