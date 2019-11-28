dfe0c5a6f879b32b464020b7b02aa9d6e09de33b
Christian <noubichristian@yahoo.fr>
2019-11-28 15:08:55 +0100

    TPP-UI. Drop down list of TPP name should disappear by clicking somewhere uninstall ng click outside test#402

--- tpp-ui/src/app/commons/navbar/navbar.component.spec.ts
+++ tpp-ui/src/app/commons/navbar/navbar.component.spec.ts
@@ -7,7 +7,6 @@ import {AuthService} from "../../services/auth.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {ReactiveFormsModule} from "@angular/forms";
 import {Router} from "@angular/router";
-import {ClickOutsideModule} from "ng-click-outside";
 
 describe('NavbarComponent', () => {
     let component: NavbarComponent;
