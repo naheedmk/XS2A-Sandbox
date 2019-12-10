3179592ea8650901ea02ebc9fc183f838b6b9a29
Hergy Tchuinkou <htc@adorsys.de>
2019-12-10 15:37:12 +0100

    added logic for auto-copy-paste of ids in devportal

--- developer-portal-ui/src/pages/test-cases/components/api-endpoints/emb-payment-init-auth-post/emb-payment-init-auth-post.component.ts
+++ developer-portal-ui/src/pages/test-cases/components/api-endpoints/emb-payment-init-auth-post/emb-payment-init-auth-post.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../../../../../services/local-storage.service';
 
 @Component({
   selector: 'app-emb-payment-init-auth-post',
-  templateUrl: './emb-payment-init-auth-post.component.html'
+  templateUrl: './emb-payment-init-auth-post.component.html',
 })
 export class EmbPaymentInitAuthPostComponent implements OnInit {
   activeSegment = 'documentation';
@@ -13,7 +14,13 @@ export class EmbPaymentInitAuthPostComponent implements OnInit {
     'PSU-IP-Address': '1.1.1.1',
   };
 
-  constructor() {}
+  paymentId: string;
+
+  constructor(public localStorageService: LocalStorageService) {
+    console.log('localstorage', this.localStorageService.get('paymentId'));
+    this.paymentId = this.localStorageService.get('paymentId');
+    console.log('ssfdfdf', this.paymentId);
+  }
 
   changeSegment(segment) {
     if (segment === 'documentation' || segment === 'play-data') {
