f19680ddddc56fc65453fc783aa8f11f4f4dc01e
may <mpo@adorsys.com.ua>
2019-12-17 08:03:59 +0200

    added endpoint for retrieving periodic payments

---
+++ online-banking/online-banking-app/src/test/java/de/adorsys/ledgers/oba/service/impl/service/PaymentServiceImplTest.java
@@ -0,0 +1,12 @@
+package de.adorsys.ledgers.oba.service.impl.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PaymentServiceImplTest {
+
+    @Test
+    public void getPeriodicPayments() {
+    }
+}
