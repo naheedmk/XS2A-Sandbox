ab45225b79b70d563bf21a2926f7c99d4de74c83
may <mpo@adorsys.com.ua>
2020-01-16 13:13:06 +0100

    extended error message in Developer portal

--- developer-portal-ui/src/pages/test-cases/components/play-with-data/play-wth-data.component.ts
+++ developer-portal-ui/src/pages/test-cases/components/play-with-data/play-wth-data.component.ts
@@ -220,4 +220,8 @@ export class PlayWthDataComponent implements OnInit {
       .getPreparedJsonData(paymentService + '/' + paymentProduct + '.json')
       .subscribe(data => (this.body = data));
   }
+
+  public getError(responseElement: any) {
+    return JSON.stringify(responseElement);
+  }
 }
