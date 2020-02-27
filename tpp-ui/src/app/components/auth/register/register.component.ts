e6d89a7c69aa2b713bd450406a5138a1ed623f98
Christian <noubichristian@yahoo.fr>
2020-02-27 14:14:49 +0100

    Tpp-ui fixes and show countries by name in the registration#572

--- tpp-ui/src/app/components/auth/register/register.component.ts
+++ tpp-ui/src/app/components/auth/register/register.component.ts
@@ -79,7 +79,10 @@ export class RegisterComponent implements OnInit {
   private initializeCountryList() {
     this.service.getCountryCodes().subscribe(
       data => {
-        this.countries = data;
+        this.countries = [];
+        Object.keys(data).forEach(countryKey => {
+          this.countries.push({code: countryKey, name: data[countryKey]});
+        })
         this.selectedCountry = '';
       },
       error => {
