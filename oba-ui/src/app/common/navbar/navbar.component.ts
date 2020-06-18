b24acbe58a62ee96d632e574a3b878f33dfd0094
dinakatereniuk <dka@adorsys.com.ua>
2020-06-18 10:19:05 +0300

    update user login on navigation without refreshing the page

--- oba-ui/src/app/common/navbar/navbar.component.ts
+++ oba-ui/src/app/common/navbar/navbar.component.ts
@@ -1,8 +1,7 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { CustomizeService } from '../services/customize.service';
-import { UserTO } from '../../api/models';
-import { OnlineBankingService } from '../services/online-banking.service';
+import { ShareDataService } from '../services/share-data.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,19 +10,19 @@ import { OnlineBankingService } from '../services/online-banking.service';
 })
 export class NavbarComponent implements OnInit, DoCheck {
   public title: string;
-  public obaUser: UserTO;
+  public obaUser;
   public toggleFlag = false;
   constructor(
     public customizeService: CustomizeService,
     private authService: AuthService,
-    private onlineBankingService: OnlineBankingService
+    private shareDataService: ShareDataService
   ) {}
 
   ngOnInit() {
     if (this.authService.isLoggedIn()) {
-      this.onlineBankingService
-        .getCurrentUser()
-        .subscribe((data) => (this.obaUser = data.body));
+      this.shareDataService.currentUser.subscribe(
+        (data) => (this.obaUser = data)
+      );
     }
   }
 
